9a2a71ee908ee1f48837863286b0f96cc063dd3b
rald <thecoconutnutisagiantnut@gmail.com>
2026-02-06 09:12:02 +0800

    OK

--- tinyscheme-sdl2/turtle.scm
+++ tinyscheme-sdl2/turtle.scm
@@ -1,3 +1,5 @@
+#!/usr/bin/env scheme
+
 (load-extension "./ts_sdl2")
 (load-extension "./ts_random")
 
@@ -7,35 +9,32 @@
 (define x 400)
 (define y 300)
 (define h -90)
-(define pen-is-down #t)
+(define is-pen-down #t)
 
 
 
 
 (define (d2r d) (/ (* d PI) 180))
 
-(define (pen-down) (set! pen-is-down #t))
+(define (pen-down) (set! is-pen-down #t))
 
-(define (pen-up)   (set! pen-is-down #f))
+(define (pen-up)   (set! is-pen-down #f))
 
 (define (set-pen-color r g b) (sdl2-set-render-draw-color renderer r g b 255))
 
 (define (move d) 
 	(define nx (+ x (* d (cos (d2r h)))))
 	(define ny (+ y (* d (sin (d2r h)))))
-	(if pen-is-down (sdl2-render-draw-line renderer x y nx ny))
+	(if is-pen-down (sdl2-render-draw-line renderer x y nx ny))
 	(set! x nx)
-	(set! y ny)
-)
+	(set! y ny))
 
 (define (turn a)
-	(set! h (+ h a))
-)
+	(set! h (+ h a)))
 
 (define (jump i j) 
 	(set! x i)
-	(set! y j)	
-)
+	(set! y j))
 
 
 
@@ -44,15 +43,13 @@
 	(do ((i 0 (+ i 1)))
 	    ((>= i 5))
 	  (move s)
-	  (turn 144))
-)
+	  (turn 144)))
 
 (define (poly sides size) 
 	(do ((i 0 (+ i 1)))
 	    ((>= i sides))
 	  (move size)
-	  (turn (/ 360 sides)))
-)
+	  (turn (/ 360 sides))))
 
 
 
@@ -73,11 +70,12 @@
 				(k (modulo (rand) 360))
 				(r (modulo (rand) 256))
 				(g (modulo (rand) 256))
-				(b (modulo (rand) 256)))
+				(b (modulo (rand) 256))
+				(s (+ (modulo (rand) 100) 10)))
 					(jump i j)
 					(turn k)
 					(set-pen-color r g b)
-					(star (+ (modulo (rand) 100) 10)))
+					(star s))
 	(loop (+ l 1)))))
 
 
