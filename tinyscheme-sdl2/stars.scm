50eefaac8cc0f584b0515c4eb7607f4b1735ba98
rald <thecoconutnutisagiantnut@gmail.com>
2026-02-06 09:44:49 +0800

    OK

---
+++ tinyscheme-sdl2/stars.scm
@@ -0,0 +1,39 @@
+#!/usr/bin/env scheme
+
+(load "turtle.scm")
+
+
+
+(define window (sdl2-create-window "Stars" 800 600))
+(define renderer (sdl2-create-renderer window -1 0))
+(sdl2-set-render-draw-color renderer 0 0 0 255)
+(sdl2-render-clear renderer)
+
+
+
+(srand (time))
+(let loop ((l 0))
+	(if (< l 10)
+		(begin
+			(let 
+				((i (modulo (rand) 800))
+				(j (modulo (rand) 600))
+				(k (modulo (rand) 360))
+				(r (modulo (rand) 256))
+				(g (modulo (rand) 256))
+				(b (modulo (rand) 256))
+				(s (+ (modulo (rand) 100) 10)))
+					(jump i j)
+					(turn k)
+					(set-pen-color r g b)
+					(star s))
+	(loop (+ l 1)))))
+
+
+ 
+(sdl2-render-present renderer)
+(sdl2-delay 5000)
+(sdl2-destroy-renderer renderer)
+(sdl2-destroy-window window)
+
+
