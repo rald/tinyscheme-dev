a619017762ed0a26831455d96ed5eddcfeaef44a
rald <thecoconutnutisagiantnut@gmail.com>
2026-03-23 16:45:52 +0800

    OK

---
+++ tinyscheme-sdl2/ts_util.c
@@ -0,0 +1,34 @@
+#include "log.h"
+#include "scm.h"
+
+#include <stdlib.h>
+#include <time.h>
+
+static pointer ts_util_time(scheme *sc, pointer args) {
+	return scm_mk_int(sc, time(NULL));
+}
+
+static pointer ts_util_srand(scheme *sc, pointer args) {
+	int seed=0;
+
+    if (scm_unpack(sc, &args, "d", &seed)) {
+        log_error("%s: %s\n", __FUNCTION__, scm_get_error());
+        return sc->NIL;
+    }
+
+    srand(seed);
+
+	return sc->T;
+}
+
+static pointer ts_util_rand(scheme *sc, pointer args) {
+	return scm_mk_int(sc, rand());
+}
+
+void init_ts_util(scheme *sc) {
+	scm_define_api_call(sc, "ts-util-time", ts_util_time);
+	scm_define_api_call(sc, "ts-util-srand", ts_util_srand);
+	scm_define_api_call(sc, "ts-util-rand", ts_util_rand);
+}
+
+
--- tinyscheme-sdl2/turtle/turtle.scm
+++ tinyscheme-sdl2/turtle/turtle.scm
@@ -1,69 +1,87 @@
-;; file: turtle.scm - WORKING VERSION
 (load-extension "../ts_sdl2")
-(load-extension "../ts_random")
+(load-extension "../ts_util")
 (load "../event-handling.scm")
 
-(sdl2-init)
+
+
 (define *running* #t)
 (define *x* 320.0)
 (define *y* 240.0)
-(define *h* 0.0)
+(define *heading* 0.0)
 (define *isPenDown* #t)
 (define *PI* 3.14159265358979323846)
 
-(define window (sdl2-create-window))
-(define renderer (sdl2-create-renderer window))
+
+
+(sdl2-init)
+
+(define *window* (sdl2-create-window))
+(define *renderer* (sdl2-create-renderer *window*))
 
 (add-event-handler sdl2-key-down
     (lambda (event-id scancode)
-        (if (= scancode sdl2-scancode-escape) (set! running #f))
+        (if (= scancode sdl2-scancode-escape) (set! *running* #f))
         #f))
 
 (add-event-handler sdl2-quit
-    (lambda (event-id) (set! running #f) #f))
+    (lambda (event-id) (set! *running* #f) #f))
 
-(define (d2r d) (* d (/ *PI* 180.0)))
+(define (d2r degrees) (* degrees (/ *PI* 180.0)))
 
-(define (turtle-move d)
-    (let* ((nx (+ *x* (* d (cos (d2r *h*)))))
-           (ny (+ *y* (* d (sin (d2r *h*))))))
-        (if *isPenDown* (sdl2-render-draw-line renderer *x* *y* nx ny))
+(define (turtle-move distance)
+    (let* ((nx (+ *x* (* distance (cos (d2r *heading*)))))
+           (ny (+ *y* (* distance (sin (d2r *heading*))))))
+        (if *isPenDown* (sdl2-render-draw-line *renderer* *x* *y* nx ny))
         (set! *x* nx)
         (set! *y* ny)))
 
-(define (turtle-turn a) (set! *h* (+ *h* a)))
+(define (turtle-turn a) (set! *heading* (+ *heading* a)))
+
+(define (turtle-jump x y)
+    (set! *x* x)
+    (set! *y* y))
 
 (define (turtle-clean)
-    (sdl2-set-render-draw-color renderer 0 0 0 255)
-    (sdl2-render-clear renderer)
-    (sdl2-render-present renderer))
+    (sdl2-set-render-draw-color *renderer* 0 0 0 255)
+    (sdl2-render-clear *renderer*)
+    (sdl2-render-present *renderer*))
 
 (define (turtle-set-color r g b a)
-    (sdl2-set-render-draw-color renderer r g b a))
+    (sdl2-set-render-draw-color *renderer* r g b a))
 
+(define (turtle-star size)
+    (do ((i 0 (+ i 1))) ((>= i 5))
+        (turtle-move size)
+        (turtle-turn 144)))
+
+
+
+(ts-util-srand (ts-util-time))
 
 (turtle-clean)
 
 (turtle-set-color
-    (modulo (rand) 255)
-    (modulo (rand) 255)
-    (modulo (rand) 255)
+    (modulo (ts-util-rand) 255)
+    (modulo (ts-util-rand) 255)
+    (modulo (ts-util-rand) 255)
     255)
 
-(do ((i 0 (+ i 1))) ((>= i 5))
-    (turtle-move 100)
-    (turtle-turn 144))
+(turtle-star 100)
 
-(sdl2-render-present renderer)
+
+(sdl2-render-present *renderer*)
+
+
 
 (do () ((not *running*))
     (let ((event (sdl2-poll-event)))
         (if (not (eq? event #f))
-            (handle-event event)))
-)
+            (handle-event event))))
 
-(sdl2-destroy-renderer renderer)
-(sdl2-destroy-window window)
+
+
+(sdl2-destroy-renderer *renderer*)
+(sdl2-destroy-window *window*)
 
 
 
