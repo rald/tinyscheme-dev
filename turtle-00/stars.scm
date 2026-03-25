f70deed5978dd67d58c5ef9a6e4ed0f5c86fa59d
rald <thecoconutnutisagiantnut@gmail.com>
2026-03-25 14:54:16 +0800

    OK

--- turtle-00/stars.scm
+++ turtle-00/stars.scm
@@ -36,9 +36,22 @@
 
 (turtle-set-pen-color 0 0 0 255)
 (turtle-clean)
-(turtle-set-pen-color 255 255 255 255)
-(draw-star 100)
 
+(do ((i 0 (+ i 1))) ((= i 100))
+    (let* ( (r (modulo (util-rand) 256))
+            (g (modulo (util-rand) 256))
+            (b (modulo (util-rand) 256))
+            (a sdl2-alpha-opaque)
+            (x (modulo (util-rand) *screen-width*))
+            (y (modulo (util-rand) *screen-height*))
+            (a (modulo (util-rand) 360))
+            (size (+ (modulo (util-rand) 50) 50)))
+        (turtle-set-pen-color r g b a)
+        (turtle-jump x y)
+        (turtle-turn a)
+        (draw-star size)
+    )
+)
 
 
 (sdl2-render-present *renderer*)
